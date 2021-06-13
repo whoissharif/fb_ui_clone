7b9a4f396375a81eba859c3ea64a0711a070b82f
whoissharif <whoissharif>
2021-06-13 08:47:30 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:facebook_ui/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Facebook Ui Clone',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/widgets/create_post_container.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:facebook_ui/models/models.dart';
+import 'package:flutter/material.dart';
+
+class CreatePostContainer extends StatelessWidget {
+  final User currentUser;
+
+  CreatePostContainer({this.currentUser});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
